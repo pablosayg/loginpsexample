1081b395f205106b7540ea755dce35e127a9b8c5
Pablo Say <pablo.davidsg42@gmail.com>
2021-10-29 20:01:22 -0600

    inicio2

--- loginpsexample/karma.conf.js
+++ loginpsexample/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/loginpsv1'),
+      dir: require('path').join(__dirname, './coverage/loginpsexample'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- loginpsexample/src/app/app.component.spec.ts
+++ loginpsexample/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'loginpsv1'`, () => {
+  it(`should have as title 'loginpsexample'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('loginpsv1');
+    expect(app.title).toEqual('loginpsexample');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('loginpsv1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('loginpsexample app is running!');
   });
 });
