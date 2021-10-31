96c4d698539c1f8e9d89f029cf1186941ce582ea
Pablo Say <pablo.davidsg42@gmail.com>
2021-10-31 01:15:57 -0600

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,9 @@ import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: "login", component: LoginComponent, pathMatch: 'full' },
-  { path: "register", redirectTo: '/register', component: RegisterComponent, pathMatch: 'full' },
-  { path: "home", component: HomeComponent, pathMatch: 'full' }];
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'register', redirectTo: '/register', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
