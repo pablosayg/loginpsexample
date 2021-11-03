42c25bea913022454fe459b6cef86ab4643551f7
Pablo Say <pablo.davidsg42@gmail.com>
2021-11-02 19:31:42 -0600

    api5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  //{ path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent, pathMatch: 'full' }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  //imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
