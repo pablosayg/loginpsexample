b2279b711170640b90c4335e13d8c8f4586ff43b
Eduardo Flores <guayof.r9@gmail.com>
2021-11-04 10:52:52 -0600

    test xD

--- pcs.backend/app/Http/Controllers/UserController.php
+++ pcs.backend/app/Http/Controllers/UserController.php
@@ -133,7 +133,7 @@ class UserController extends Controller
 
     public function login(Request $request){
 
-        //$jwtAut = new \App\Helpers\JwtAuth();        
+        /*//$jwtAut = new \App\Helpers\JwtAuth();        
         $jwtAut = new \JwtAuth();
 
         // Recibir datos por POST
@@ -174,8 +174,8 @@ class UserController extends Controller
                 'code' => 200,
                 'message' => 'Los datos enviados no son correctos'
             );
-        }
-        //$signup = 'token hola pablito';
+        }*/
+        $signup = 'token hola pablito';
         return response()->json($signup, 200);
     }
 
